a72da21fccee4336db783811b163d5705885e368
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-29 14:30:22 -0300

    Developed component room-details

--- client-room/src/app/room-details/room-details.component.ts
+++ client-room/src/app/room-details/room-details.component.ts
@@ -1,4 +1,9 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Room } from '../room';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-room-details',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomDetailsComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  room: Room;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private roomService: RoomService
+  ) { }
 
   ngOnInit(): void {
+    this.room = new Room();
+    this.id = this.route.snapshot.params["id"];
+
+    this.roomService.getRoom(this.id);
+      .subscribe(
+        (data) => {
+          console.log(data);
+          this.room = data;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 
+  list() {
+    this.router.navigate(["rooms"])
+  }
 }
