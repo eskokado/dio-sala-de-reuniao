b79934f2c63a4ab7d1f62a3d802ee5e2e3c94906
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-29 12:22:17 -0300

    Preparing project (bootstrap, jquery) and created components and service

--- client-room/src/app/app.module.ts
+++ client-room/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { RoomListComponent } from './room-list/room-list.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent,
+    RoomDetailsComponent,
+    RoomListComponent,
+    UpdateRoomComponent
   ],
   imports: [
     BrowserModule
