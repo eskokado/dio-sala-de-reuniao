fe0b257e3308300e024771d840bd9960a30aa6cf
Edson Shideki Kokado <eskokado@gmail.com>
2021-07-10 09:51:39 -0300

    Initial test list Rooms

---
+++ client-room/cypress/integration/app-init.spec.ts
@@ -0,0 +1,20 @@
+﻿import roomsFixture from '../fixtures/rooms.json'
+
+describe('test this fixture json utf8', ()=> {
+  it('test this', () => {
+    cy
+      .fixture('rooms', 'utf8')
+      .then( rooms => {
+        expect(roomsFixture, 'the same data').to.deep.equal(rooms)
+        expect(roomsFixture[0].name).to.eq(rooms[0].name)
+      })
+  })
+})
+
+describe('test this intercept', () => {
+  it('3 data', () => {
+    cy.intercept('GET', '**/rooms', roomsFixture)
+    cy.visit('/')
+    cy.get('table').contains('td', roomsFixture[0].name);
+  })
+})
