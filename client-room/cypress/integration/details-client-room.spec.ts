57290df6df0ed92d2644b7cc81b187ecd9604a1f
Edson Shideki Kokado <eskokado@gmail.com>
2021-07-10 09:52:46 -0300

    Test details room

---
+++ client-room/cypress/integration/details-client-room.spec.ts
@@ -0,0 +1,25 @@
+﻿describe('Details Client Room', () => {
+  const name = "Reunião de PDCA"
+  const date = "05-07-2021"
+  const startHour = "12:00:00"
+  const endHour = "13:30:00"
+  
+  it('Get data', () => {
+    cy.visit('/')
+    cy.get('table').contains('td', name);
+    cy.get('table').contains('td', date);
+    cy.get('table').contains('td', startHour);
+    cy.get('table').contains('td', endHour);
+  })
+
+  it('Click button Details Room', () => {
+    cy.contains('Details').click();
+  })
+
+  it('Verify fields details', () => {
+    cy.get('div').contains(name);
+    cy.get('div').contains(date);
+    cy.get('div').contains(startHour);
+    cy.get('div').contains(endHour);
+  })
+})
