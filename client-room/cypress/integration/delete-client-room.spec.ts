09e6981be7b4efe921d4234cdd1d9013d54aaddc
Edson Shideki Kokado <eskokado@gmail.com>
2021-07-10 09:53:52 -0300

    Test delete room

---
+++ client-room/cypress/integration/delete-client-room.spec.ts
@@ -0,0 +1,18 @@
+﻿describe('Update Client Room', () => {
+  const name = "Nova reunião alterado"
+  const date = "18/08/2021"
+  const startHour = "15:00:00"
+  const endHour = "17:00:00"
+
+  it('Get data', () => {
+    cy.visit('/')
+    cy.get('table').contains('td', name);
+    cy.get('table').contains('td', date);
+    cy.get('table').contains('td', startHour);
+    cy.get('table').contains('td', endHour);
+  })
+
+  it('Click button Delete Room', () => {
+    cy.contains('Delete').click();
+  })
+})
